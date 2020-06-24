002ee6d02d4efead0fa85325341e8e26570ac266
safeentry-qr <67377639+safeentry-qr@users.noreply.github.com>
2020-06-24 15:03:08 +0000

    Fixed check-out time

--- src/remove.js
+++ src/remove.js
@@ -2,7 +2,8 @@ if (localStorage.time == undefined || localStorage.place == undefined) {
     window.location = "index.html";
 }
 else {
-    document.getElementById("time").appendChild(document.createTextNode(localStorage.time));
+    const time = new Date();
+    document.getElementById("time").appendChild(document.createTextNode(`${time.getDate()} ${["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec"][time.getMonth()]} ${time.getFullYear()}, ${time.getHours() >= 12 ? (time.getHours() == 12 ? 12 : time.getHours() - 12) : (time.getHours() == 0 ? 12 : time.getHours())}:${time.getMinutes() < 10 ? `0${time.getMinutes()}` : time.getMinutes()} ${time.getHours() >= 12 ? "PM" : "AM"}`));
     document.getElementById("place").appendChild(document.createTextNode(localStorage.place));
     localStorage.clear();
 }
